1c447f0ed5755e403ab83ec311bb80f8303b8ac1
tejachowdary0 <141829994+tejachowdary0@users.noreply.github.com>
2023-11-06 16:48:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001910675865'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'godmode_support')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1002113538713')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
